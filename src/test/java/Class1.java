0bbea14904fe1575a11993b66b60068fb11d224c
sanjay1506 <sanjaysai227@gmail.com>
2022-09-18 14:28:58 +0530

    message

---
+++ src/test/java/Class1.java
@@ -0,0 +1,18 @@
+
+public class Class1 {
+	private void m1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void m3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
